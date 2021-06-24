5bbbaf92ff33ca78025d6083359829f241eb129b
corokada <okada@core-tech.jp>
2021-06-24 17:39:17 +0900

    Update service_restart.sh

--- service_restart.sh
+++ service_restart.sh
@@ -7,12 +7,21 @@
 #
 
 if [ -f /etc/httpd/certs/restart_apache ]; then
-    /etc/init.d/httpd graceful
+    if [ -f /etc/init.d/httpd ]; then
+        /etc/init.d/httpd graceful
+    else
+        /bin/systemctl reload httpd
+    fi
     rm -rf /etc/httpd/certs/restart_apache
 fi
 
 if [ -f /etc/httpd/certs/restart_mail ]; then
-    /etc/init.d/postfix reload
-    /etc/init.d/dovecot reload
+    if [ -f /etc/init.d/postfix ]; then
+        /etc/init.d/postfix reload
+        /etc/init.d/dovecot reload
+    else
+        /bin/systemctl reload postfix
+        /bin/systemctl reload dovecot
+    fi
     rm -rf /etc/httpd/certs/restart_mail
 fi
